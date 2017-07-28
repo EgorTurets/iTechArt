0aa72aac200f1c7bad4e8c615bc5cdcea0f9c0e8
EgorTurets <EgorTurets@gmail.com>
2017-07-28 11:29:04 +0300

    Points 6, 8-9 is done

--- P4/TaskQueries.sql
+++ P4/TaskQueries.sql
@@ -4,18 +4,22 @@ GO
 /*Problem 1*/
 SELECT * FROM Product
 
+
 /*Problem 2*/
 SELECT ProductID, ProductName FROM Product 
 WHERE Price <= 500
+
 
 /*Problem 3*/
 SELECT AVG(Amount) FROM [Order]
 WHERE OrderDate > '01-01-2015' AND OrderDate < '01-31-2015'
 
+
 /*Problem 4*/
 SELECT Product.ProductName, DATEDIFF(YEAR, [Order].OrderDate, GETDATE()) AS Age
 FROM Product INNER JOIN [Order] ON Product.ProductID = [Order].FK_ProductID
 WHERE Product.Price = (SELECT MAX(Price) FROM Product)
+
 
 /*Problem 5*/
 SELECT DISTINCT Product.ProductName
@@ -24,40 +28,19 @@ WHERE [Order].OrderDate > '01-01-2015' AND [Order].OrderDate < '01-31-2015'
 
 
 /*Problem 6*/
-SELECT Jan2015.ProductName
+SELECT Dec2014.ProductName
 FROM (
 	SELECT Product.ProductID, Product.ProductName, SUM([Order].Quantity) AS Quantity
-	FROM Product LEFT JOIN [Order] ON Product.ProductID = [Order].FK_ProductID
+	FROM Product FULL JOIN [Order] ON Product.ProductID = [Order].FK_ProductID
 	WHERE MONTH([Order].OrderDate) = 1 AND YEAR([Order].OrderDate) = 2015
 	GROUP BY Product.ProductID, Product.ProductName) AS Jan2015
-	FULL JOIN (
+FULL JOIN (
 	SELECT  Product.ProductID, Product.ProductName, SUM([Order].Quantity) AS Quantity
 	FROM Product INNER JOIN [Order] ON Product.ProductID = [Order].FK_ProductID
 	WHERE MONTH([Order].OrderDate) = 12 AND YEAR([Order].OrderDate) = 2014
 	GROUP BY Product.ProductID, Product.ProductName) AS Dec2014
 	ON Jan2015.ProductID = Dec2014.ProductID
-	--RIGHT JOIN Product ON Jan2015.ProductID = Product.ProductID
-WHERE Jan2015.Quantity < Dec2014.Quantity OR Jan2015.ProductID <> Dec2014.ProductID
-
-
-
-
-
-SELECT [Order].FK_ProductID, COUNT([Order].Quantity) AS Quantity, YMParse.[Month], YMParse.[Year]	
-FROM [Order] INNER JOIN (
-	SELECT Product.ProductID, [Order].FK_ProductID, MONTH([Order].OrderDate) AS [Month], YEAR([Order].OrderDate) AS [Year]
-	FROM [Order] FULL JOIN Product ON [Order].FK_ProductID = Product.ProductID
-	WHERE MONTH([Order].OrderDate) = 1 AND YEAR([Order].OrderDate) = 2015
-	) AS YMParse ON [Order].FK_ProductID = YMParse.FK_ProductID
-GROUP BY YMParse.[Month], [Year], [Order].FK_ProductID
-
-SELECT * FROM [Order]
-
-SELECT [Order].FK_ProductID, MONTH([Order].OrderDate) AS [Month], YEAR([Order].OrderDate) AS [Year]
-	FROM [Order]
-
-
-
+WHERE Jan2015.Quantity < Dec2014.Quantity OR Jan2015.Quantity IS NULL
 
 
 /*Problem 7*/
@@ -66,4 +49,24 @@ FROM Shop INNER JOIN ShopProductRelation ON Shop.ShopID = ShopProductRelation.FK
 	INNER JOIN Product ON ShopProductRelation.FK_ProductID = Product.ProductID
 GROUP BY Shop.ShopName
 
+
 /*Problem 8*/
+SELECT Shop.ShopName, Sales.SumAmount
+FROM (
+	SELECT Shop.ShopID, Shop.ShopName, SUM([Order].Amount) AS SumAmount, YEAR([Order].OrderDate) AS OrderYear
+	FROM Shop INNER JOIN [Order] ON Shop.ShopID = [Order].FK_ShopID
+	WHERE YEAR([Order].OrderDate) = 2015
+	GROUP BY Shop.ShopID, Shop.ShopName, YEAR([Order].OrderDate)
+	) AS Sales
+FULL OUTER JOIN Shop ON Sales.ShopID = Shop.ShopID
+
+
+/*Problem 9*/
+SELECT Customer.CustomerName, CustomerPurchases.OrderSum
+FROM Customer INNER JOIN (
+	SELECT OrderCustomerRelation.FK_CustomerID AS CustomerID, SUM([Order].Amount) AS OrderSum
+	FROM OrderCustomerRelation INNER JOIN [Order] ON OrderCustomerRelation.FK_OrderID = [Order].OrderID
+	WHERE YEAR([Order].OrderDate) = 2014
+	GROUP BY OrderCustomerRelation.FK_CustomerID
+) AS CustomerPurchases ON Customer.CustomerID = CustomerPurchases.CustomerID
+WHERE CustomerPurchases.OrderSum >= 1000000
