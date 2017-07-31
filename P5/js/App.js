d396d92277cae5209b837c875ad0245aa8fe3746
EgorTurets <EgorTurets@gmail.com>
2017-07-31 18:26:34 +0300

    Add redux pac

---
+++ P5/js/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+// import { createStore, renderDevTools } from '../utils/devTools';
+
+import FriendListApp from './FriendListApp';
+import * as reducers from '../reducers';
+
+const reducer = combineReducers(reducers);
