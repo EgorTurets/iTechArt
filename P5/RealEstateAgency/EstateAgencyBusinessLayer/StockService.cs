93036de41fe41215b2eb72b9718b5d59c6213d58
EgorTurets <EgorTurets@gmail.com>
2017-08-22 15:57:29 +0300

    implemented StockService and StockRepository

---
+++ P5/RealEstateAgency/EstateAgencyBusinessLayer/StockService.cs
@@ -0,0 +1,66 @@
+﻿using RealEstateAgency.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealEstateAgency.Models.Models;
+using RealEstateAgency.DBLayer;
+
+namespace RealEstateAgency.BusinessLayer
+{
+    public class StockService : IStockService
+    {
+        private IStockRepository _repository;
+
+        public StockService(IStockRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Notification AddNotification(Notification notification)
+        {
+            return _repository.AddNotification(notification);
+        }
+
+        public async Task<Notification> AddNotificationAsync(Notification notification)
+        {
+            return await _repository.AddNotificationAsync(notification);
+        }
+
+        public IEnumerable<Notification> GetAllUserNotifications(int userID)
+        {
+            return _repository.GetAllUserNotifications(userID);
+        }
+
+        public async Task<IEnumerable<Notification>> GetAllUserNotificationsAsync(int userID)
+        {
+            return await _repository.GetAllUserNotificationsAsync(userID);
+        }
+
+        public Notification GetNotification(int id)
+        {
+            return _repository.GetNotification(id);
+        }
+
+        public async Task<Notification> GetNotificationAsync(int id)
+        {
+            return await _repository.GetNotificationAsync(id);
+        }
+
+        public bool RemoveNotification(int id)
+        {
+            return _repository.RemoveNotification(id);
+        }
+
+        public IEnumerable<Notification> SearchNotifications(decimal minPrice, decimal maxPrice, double minMetric, double maxMetric, bool forRent)
+        {
+            return _repository.SearchNotifications(minPrice, maxPrice, minMetric, maxMetric, forRent);
+        }
+
+        public async Task<IEnumerable<Notification>> SearchNotificationsAsync(decimal minPrice, decimal maxPrice, double minMetric, double maxMetric, bool forRent)
+        {
+            return await _repository.SearchNotificationsAsync(minPrice, maxPrice, minMetric, maxMetric, forRent);
+        }
+    }
+}
---
+++ P5/RealEstateAgency/EstateAgencyDBLayer/StockRepository.cs
@@ -0,0 +1,88 @@
+﻿using RealEstateAgency.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealEstateAgency.Models.Models;
+using System.Transactions;
+
+namespace RealEstateAgency.DBLayer
+{
+    public class StockRepository : IStockRepository
+    {
+        private EstateAgencyDbContext notificationContext;
+
+        public StockRepository()
+        {
+            notificationContext = new EstateAgencyDbContext();
+        }
+
+        public Notification AddNotification(Notification notification)
+        {
+            notificationContext.Set<Notification>().Add(notification);
+            notificationContext.SaveChanges();
+            return notification;
+        }
+
+        public Task<Notification> AddNotificationAsync(Notification notification)
+        {
+            return Task.Factory.StartNew(() => AddNotification(notification));
+        }
+
+        public IEnumerable<Notification> GetAllUserNotifications(int userID)
+        {
+            return notificationContext.Notifications.Where(n => n.ProprietorID == userID);
+        }
+
+        public Task<IEnumerable<Notification>> GetAllUserNotificationsAsync(int userID)
+        {
+            return Task.Factory.StartNew(() => GetAllUserNotifications(userID));
+        }
+
+        public Notification GetNotification(int id)
+        {
+            return notificationContext.Notifications.FirstOrDefault(n => n.NoticeID == id);
+        }
+
+        public Task<Notification> GetNotificationAsync(int id)
+        {
+            return Task.Factory.StartNew(() => GetNotification(id));
+        }
+
+        public bool RemoveNotification(int id)
+        {
+            using (var tran = new TransactionScope(TransactionScopeOption.Required))
+            {
+                try
+                {
+                    Notification notice = GetNotification(id);
+                    notificationContext.Notifications.Remove(notice);
+                    notificationContext.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+
+            }
+        }
+
+        public IEnumerable<Notification> SearchNotifications(decimal minPrice, decimal maxPrice, double minMetric, double maxMetric, bool forRent)
+        {
+            return notificationContext.Notifications.Where(n => (n.Price >= minPrice) && (n.Price <= maxPrice) &&
+                                                                (n.Metric >= minMetric) && (n.Metric <= maxMetric) && (n.ForRent == forRent));
+        }
+
+        public Task<IEnumerable<Notification>> SearchNotificationsAsync(decimal minPrice, decimal maxPrice, double minMetric, double maxMetric, bool forRent)
+        {
+            return Task.Factory.StartNew(() => SearchNotifications(minPrice, maxPrice, minMetric, maxMetric, forRent));
+        }
+
+        public void Dispose()
+        {
+            notificationContext.Dispose();
+        }
+    }
+}
