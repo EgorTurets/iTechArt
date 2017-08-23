aedff34aab11b39d392eb271428f76795d798a44
EgorTurets <EgorTurets@gmail.com>
2017-08-23 16:06:06 +0300

    FindByIdAsync, FindByNameAsync, CheckPasswordAsync, CreateAsync, ChangePasswordAsync methods was implement

--- P5/RealEstateAgency/RealEstateAgency.UI/Utils/CustomUserManager.cs
+++ P5/RealEstateAgency/RealEstateAgency.UI/Utils/CustomUserManager.cs
@@ -14,30 +14,79 @@ namespace RealEstateAgency.UI.Utils
 {
     public class CustomUserManager : UserManager<CustomUser, int>
     {
+        private IUserStore<CustomUser, int> _store;
+
         public CustomUserManager(IUserStore<CustomUser, int> store) : base(store)
         {
+            _store = store;
         }
 
         public static CustomUserManager Create(IdentityFactoryOptions<CustomUserManager> options, IOwinContext context)
-        {
+        {            
             var manager = new CustomUserManager(new CustomUserStore(context.Get<AppIdentityDbContext>()));
 
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 8
             };
+            
 
             return manager;
         }
 
-        public override Task<CustomUser> FindAsync(string userName, string password)
+
+        //=======Redefinition of necessary methods=======
+
+        public override Task<CustomUser> FindByIdAsync(int userId)
         {
-            return base.FindAsync(userName, password);
+
+            return Task.Factory.StartNew(() => this.Users.FirstOrDefault(u => u.Id == userId));
         }
 
-        public override Task<IdentityResult> CreateAsync(CustomUser user)
+        public override Task<CustomUser> FindByNameAsync(string userName)
         {
-            return base.CreateAsync(user);
+
+            return Task.Factory.StartNew(() => this.Users.FirstOrDefault(u => u.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase)));
         }
+
+        public override Task<bool> CheckPasswordAsync(CustomUser user, string password)
+        {
+            string passwordHash = this.PasswordHasher.HashPassword(password);
+
+            return Task<bool>.Factory.StartNew((() => passwordHash.Equals(user.PasswordHash)));
+        }
+
+
+        //===Add an add-on result check===
+        public override Task<IdentityResult> CreateAsync(CustomUser user, string password)
+        {
+            if (this.Users.FirstOrDefault(u => u.UserName.Equals(user.UserName, StringComparison.OrdinalIgnoreCase)) != null)
+            {
+                IdentityResult falledResult = new IdentityResult("A user with this Email already exists");
+
+                return Task.FromResult(falledResult);
+            }
+            user.PasswordHash = this.PasswordHasher.HashPassword(password);
+
+            _store.CreateAsync(user);
+
+            return Task.FromResult(IdentityResult.Success);
+        }
+
+        public override Task<IdentityResult> ChangePasswordAsync(int userId, string currentPassword, string newPassword)
+        {
+            CustomUser user = FindByIdAsync(userId).Result;
+            if (CheckPasswordAsync(user, currentPassword).Result)
+            {
+                user.PasswordHash = this.PasswordHasher.HashPassword(newPassword);
+
+                return Task.FromResult(IdentityResult.Success);
+            }
+
+            return Task.FromResult(new IdentityResult("Invalid password"));
+        }
+
+
+
     }
 }
