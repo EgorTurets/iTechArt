8f5ae496d5b85b8d069f291b7cd20e988dafa0c1
EgorTurets <EgorTurets@gmail.com>
2017-08-23 16:45:03 +0300

    The UpdateAsync and DeleteAsync methods was overridden in CustomUserStore.

--- P5/RealEstateAgency/RealEstateAgency.UI/Utils/CustomUserStore.cs
+++ P5/RealEstateAgency/RealEstateAgency.UI/Utils/CustomUserStore.cs
@@ -8,6 +8,7 @@ using RealEstateAgency.Models.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
+using System.Transactions;
 
 namespace RealEstateAgency.UI.Utils
 {
@@ -25,12 +26,43 @@ namespace RealEstateAgency.UI.Utils
         public override Task CreateAsync(CustomUser user)
         {
             if (_context.Users.FirstOrDefault(u => u.UserName.Equals(user.UserName, StringComparison.OrdinalIgnoreCase)) != null)
-
+            {
                 return Task.FromResult(false);
-
+            }
             _context.Set<CustomUser>().Add(user);
 
             return _context.SaveChangesAsync();
+        }
+
+        public override Task UpdateAsync(CustomUser user)
+        {
+            using (var tran = new TransactionScope(TransactionScopeOption.Required))
+            {
+                CustomUser userNote = _context.Users.FirstOrDefault(u => u.Id == user.Id);
+                if (userNote == null)
+                {
+                    return Task.FromResult(false);
+                }
+                userNote = user;
+                _context.Entry(userNote).State = System.Data.Entity.EntityState.Modified;
+
+                return _context.SaveChangesAsync();
+            }
+        }
+
+        public override Task DeleteAsync(CustomUser user)
+        {
+            using (var tran = new TransactionScope(TransactionScopeOption.Required))
+            {
+                CustomUser userNote = _context.Users.FirstOrDefault(u => u.Id == user.Id);
+                if (userNote == null)
+                {
+                    return Task.FromResult(false);
+                }
+                _context.Users.Remove(userNote);
+
+                return _context.SaveChangesAsync();
+            }
         }
     }
 }
