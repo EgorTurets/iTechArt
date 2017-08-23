ec4fe14381f75fa542293af244c9b743c3009d34
EgorTurets <EgorTurets@gmail.com>
2017-08-23 15:23:53 +0300

    The CreateAsync method was overridden in CustomUserStore.

--- P5/RealEstateAgency/RealEstateAgency.UI/Utils/CustomUserStore.cs
+++ P5/RealEstateAgency/RealEstateAgency.UI/Utils/CustomUserStore.cs
@@ -13,8 +13,24 @@ namespace RealEstateAgency.UI.Utils
 {
     public class CustomUserStore : UserStore<CustomUser, IdentityRole<int, IdentityUserRole<int>>, int, IdentityUserLogin<int>, IdentityUserRole<int>, IdentityUserClaim<int>>
     {
+        private AppIdentityDbContext _context;
+
         public CustomUserStore(AppIdentityDbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        //CreateAsync return 2 different types (bool || int)
+
+        public override Task CreateAsync(CustomUser user)
+        {
+            if (_context.Users.FirstOrDefault(u => u.UserName.Equals(user.UserName, StringComparison.OrdinalIgnoreCase)) != null)
+
+                return Task.FromResult(false);
+
+            _context.Set<CustomUser>().Add(user);
+
+            return _context.SaveChangesAsync();
         }
     }
 }
